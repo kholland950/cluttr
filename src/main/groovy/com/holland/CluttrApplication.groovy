25834e44772c6539a01dce82f25eab45e86f46df
Kevin Holland <hollandk@oclc.org>
2016-09-02 23:20:03 -0400

    Init commit

---
+++ src/main/groovy/com/holland/CluttrApplication.groovy
@@ -0,0 +1,12 @@
+package com.holland
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CluttrApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CluttrApplication, args
+	}
+}
