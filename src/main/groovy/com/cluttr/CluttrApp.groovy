1dc91029b4e66855ff6d674f748c557cb52b8b0c
Kevin Holland <hollandk@oclc.org>
2016-09-03 10:26:59 -0400

    Removed spring security

--- src/main/groovy/com/cluttr/CluttrApp.groovy
+++ src/main/groovy/com/cluttr/CluttrApp.groovy
@@ -1,12 +1,11 @@
-package com.holland
+package com.cluttr
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class CluttrApplication {
-
+class CluttrApp {
 	static void main(String[] args) {
-		SpringApplication.run CluttrApplication, args
+		SpringApplication.run CluttrApp, args
 	}
 }
